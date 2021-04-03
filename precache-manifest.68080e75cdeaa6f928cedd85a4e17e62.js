446c485f00d6d42559b6bfaadceb1fbac4320720
celeis <gonca.negis@gmail.com>
2021-04-03 15:08:45 +0300

    first commit

---
+++ precache-manifest.68080e75cdeaa6f928cedd85a4e17e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee5d2564dfdc0269e2f563c602b8425",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c800da6cc7c77e9eef20",
+    "url": "/static/css/main.61b5f690.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c800da6cc7c77e9eef20",
+    "url": "/static/js/main.9d00b28c.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
